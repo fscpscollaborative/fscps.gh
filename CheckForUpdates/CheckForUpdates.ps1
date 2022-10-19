992b94742660b9e5b969ca55dd803e05c79ebae3
ciellosinc <ciellosinc@users.noreply.github.com>
2022-10-19 14:27:02 +0000

    checkout

--- CheckForUpdates/CheckForUpdates.ps1
+++ CheckForUpdates/CheckForUpdates.ps1
@@ -219,8 +219,14 @@ try {
 
                 if(($fileName -eq "import.yml") -and $type -eq "FSCM")
                 {
-                    if(Test-Path -Path (Join-Path $baseFolder "PackagesLocalDirectory")){ 
-                        $removeFiles += @{ "DstFile" = Join-Path $dstPath $filename; "content" = $srcContent }
+                    if(Join-Path $dstPath $filename)
+                    {
+                        if(Test-Path -Path (Join-Path $baseFolder "PackagesLocalDirectory")){ 
+                            $removeFiles += @{ "DstFile" = Join-Path $dstPath $filename; "content" = $srcContent }
+                        }
+                    }
+                    else {
+                        return
                     }
                 }
 
