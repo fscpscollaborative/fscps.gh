0037050b185aa9a66b9a88c0ea3a5aec34f5dc8f
Florian Hopfner <florian.hopfner@inway.de>
2024-02-03 18:43:49 +0100

    Remove custom Nuget feed name option from packages.config generation

--- FSC-PS-Helper.ps1
+++ FSC-PS-Helper.ps1
@@ -590,13 +590,7 @@ function GeneratePackagesConfig
     #generate nuget.config
     $NugetConfigFileName = 'nuget.config'
     $NewNugetFile = Join-Path $NugetFolderPath $NugetConfigFileName
-    if($NugetFeedName)
-    {
-        $tempFile = (Get-Content $NugetConfigFileName).Replace('NugetFeedName', $NugetFeedName).Replace('NugetSourcePath', $NugetSourcePath)
-    }
-    else {
-        $tempFile = (Get-Content $NugetConfigFileName).Replace('<add key="NugetFeedName" value="NugetSourcePath" />', '')
-    }
+    $tempFile = (Get-Content $NugetConfigFileName).Replace('<add key="NugetFeedName" value="NugetSourcePath" />', '')
     Set-Content $NewNugetFile $tempFile
 
     #generate packages.config
