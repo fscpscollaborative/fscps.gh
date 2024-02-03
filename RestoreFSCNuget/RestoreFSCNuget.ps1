dd1c1a3bc3264a9421cb1023046dcb42149838f8
Florian Hopfner <florian.hopfner@inway.de>
2024-02-03 16:40:16 +0100

    Add GitHub action to restore FSC Nugets

---
+++ RestoreFSCNuget/RestoreFSCNuget.ps1
@@ -0,0 +1,31 @@
+Param(
+    [Parameter(HelpMessage = "DynamicsVersion", Mandatory = $true)]
+    [string] $DynamicsVersion
+)
+$ErrorActionPreference = "Stop"
+Set-StrictMode -Version 2.0
+# IMPORTANT: No code that can fail should be outside the try/catch
+
+try {
+    $helperPath = Join-Path -Path $PSScriptRoot -ChildPath "..\FSC-PS-Helper.ps1" -Resolve
+    . $helperPath
+
+    Write-Output "::group::Download default NuGet packages"
+    OutputInfo "======================================== Download default NuGet"
+
+    Update-FSCNuGet -sdkVersion $DynamicsVersion
+    Write-Output "::endgroup::"
+
+    Write-Output "::group::Nuget install packages"
+    OutputInfo "======================================== Nuget install packages"
+
+    nuget restore -PackagesDirectory ..\NuGets
+    Write-Output "::endgroup::"
+}
+catch {
+  OutputError -message $_.Exception.Message
+}
+finally
+{
+  OutputInfo "Execution is done."
+}
